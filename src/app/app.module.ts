841b8539bf5f0bd73d0c5bc40cbd90d91ac1115b
Krystian <maniak0909@gmail.com>
2023-08-29 23:32:40 +0200

    pwa update test
    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -17,6 +17,7 @@ import { BuytokensComponent } from './buytokens/buytokens.component';
 import { QRCodeModule } from 'angularx-qrcode';
 import { PayqrcodeComponent } from './payqrcode/payqrcode.component';
 import { RemoveTokenComponent } from './remove-token/remove-token.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
@@ -39,7 +40,13 @@ import { RemoveTokenComponent } from './remove-token/remove-token.component';
     ReactiveFormsModule,
     HttpClientModule,
     ToastrModule.forRoot(),
-    QRCodeModule
+    QRCodeModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
